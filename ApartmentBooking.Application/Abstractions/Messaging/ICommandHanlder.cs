011c654b840fd1eb2832100909084950fdda3b47
ibrahim <ihany941@gmail.com>
2025-02-20 17:31:52 +0200

    add request and handler interfaces for cqrs

---
+++ ApartmentBooking.Application/Abstractions/Messaging/ICommandHanlder.cs
@@ -0,0 +1,16 @@
+﻿using ApartmentBooking.Domain.Abstraction;
+using MediatR;
+
+namespace ApartmentBooking.Application.Abstractions.Messaging;
+
+// without response
+public interface ICommandHanlder<TCommand>:IRequestHandler<TCommand,Result>
+    where TCommand : ICommand
+{
+}
+
+// with response
+public interface ICommandHanlder<TCommand,TResponse> : IRequestHandler<TCommand, Result<TResponse>>
+    where TCommand : ICommand<TResponse>
+{
+}
